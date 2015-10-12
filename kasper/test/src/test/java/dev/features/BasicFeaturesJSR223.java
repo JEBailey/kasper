c59ea1d94e7a9ab35170687ddeec227e9a821b38
jebailey <jason.bailey@24601.org>
2015-10-12 06:23:23 -0400

    addin test project to provide post compilation tests to kasper

---
+++ kasper/test/src/test/java/dev/features/BasicFeaturesJSR223.java
@@ -0,0 +1,68 @@
+package dev.features;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import org._24601.kasper.core.KasperContext;
+import org._24601.kasper.error.KasperException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BasicFeaturesJSR223 {
+	
+	private KasperContext context;
+
+    @Before
+    public void setUp() throws KasperException {
+		//context = PoslProvider.getDefaultContext();
+    }
+
+	@Test
+	public void testTagCreation() throws KasperException {
+		assertEquals("<a></a>", eval("a"));
+	}
+	
+	@Test
+	public void testChildElements() throws KasperException {
+		assertEquals("<a><b></b></a>", eval("a() {b}"));
+	}
+	
+	@Test
+	public void testDiscreteElements() throws KasperException {
+		assertEquals("<a><b></b></a>", eval("a {b}"));
+	}
+	
+	@Test
+	public void testline() throws KasperException {
+		assertEquals("<a>some content</a>", eval("a \"some content\""));
+	}
+	
+	@Test
+	public void testLink() throws KasperException {
+		assertEquals("<a href='./foo' src='http://gotohere'></a>", eval("a(href = \"./foo\", src=\"http://gotohere\")"));
+	}
+	
+	@Test
+	public void testDoctype() throws KasperException {
+		assertEquals("<!doctype html>", eval("doctype(html)"));
+	}
+	
+	@Test
+	public void testComment() throws KasperException {
+		assertEquals("<!--this is a child <br>this is another-->", eval("comment{\"this is a child \"\nbr\n\"this is another\"}"));
+	}
+	
+	private Object eval(String expression) throws KasperException {
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByExtension("ksp");
+        try {
+			return engine.eval(expression);
+		} catch (ScriptException e) {
+			return e.getMessage();
+		}
+    }
+
+}
